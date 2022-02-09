d25e66a92ed4099377139438bc69dbe713da209d
Levy Nunes <levy.vix@gmail.com>
2022-02-09 07:58:25 -0300

    fix numpy error

--- python/card-games/lists.py
+++ python/card-games/lists.py
@@ -1,4 +1,24 @@
-import numpy as np
+
+# TODO convert np mean to mean function
+
+def mean(hand):
+    """
+
+    :param hand: list - cards in hand.
+    :return: float - mean value of the cards in the hand.
+    """
+
+    return sum(hand) / len(hand)
+
+
+def median(hand):
+    """
+
+    :param hand: list - cards in hand.
+    :return: float - median value of the cards in the hand.
+    """
+
+    return sorted(hand)[len(hand) // 2]
 
 
 def get_rounds(number):
@@ -40,7 +60,7 @@ def card_average(hand):
     :return:  float - average value of the cards in the hand.
     """
 
-    return np.mean(hand)
+    return mean(hand)
 
 
 def approx_average_is_average(hand):
@@ -50,7 +70,7 @@ def approx_average_is_average(hand):
     :return: bool - if approximate average equals to the `true average`.
     """
 
-    return np.mean(hand) in [np.mean(hand[::len(hand)-1]), np.median(hand[1:-1])]
+    return mean(hand) in [mean(hand[::len(hand)-1]),  median(hand[1:-1])]
 
 
 def average_even_is_average_odd(hand):
@@ -60,7 +80,7 @@ def average_even_is_average_odd(hand):
     :return: bool - are even and odd averages equal?
     """
 
-    return np.mean(hand[::2]) == np.mean(hand[1::2])
+    return mean(hand[::2]) == mean(hand[1::2])
 
 
 def maybe_double_last(hand):
@@ -71,35 +91,3 @@ def maybe_double_last(hand):
     """
 
     return hand[:-1] + [22] if hand[-1] == 11 else hand
-
-
-print(approx_average_is_average([1, 2, 3]))
-print(approx_average_is_average([2, 3, 4, 8, 8]))
-
-
-hand = [2, 3, 4, 8, 8]
-print(np.mean(hand[::len(hand)-1]))
-print(np.median(hand[1:-1]))
-
-
-text = 'Print only the words that start with s in this sentence'
-
-
-print([word for word in text.split(' ') if word.startswith('s')])
-
-
-print(list(range(0, 10, 2)))
-
-print([x for x in range(50) if x % 3 == 0])
-
-text2 = 'Print every word in this sentence that has an even number of letters'
-
-print([f'{word} é Par' for word in text2.split(' ') if len(word) % 2 == 0])
-
-print('\n')
-# fizz buzz - print numbers from 1 to 100.
-print(["FizzBuzz" if number % 3 == 0 and number % 5 == 0 else "Fizz" if number %
-      3 == 0 else "Buzz" if number % 5 == 0 else number for number in range(1, 101)])
-
-text3 = 'Create a list of the first letters of every word in this string'
-print([word[0] for word in text3.split(' ')])
